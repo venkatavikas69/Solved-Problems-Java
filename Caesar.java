ec3f65b80eb5145f3f28db53010cb97657d6803f
venkata vikas <52953086+venkatavikas69@users.noreply.github.com>
2020-11-22 16:44:26 +0530

    Update Caesar.java

--- Caesar.java
+++ Caesar.java
@@ -20,16 +20,20 @@ public class Caesar {
 				if (c >= 65 && c <= 90) { // checking if it is uppercase
 					int newkey = num + key; // adding key given
 					if (newkey > 90) { // checking if its out of range
-						int sub = (newkey - 90) + 64; // rotating the digit for ex: if letter z and key is 2 then new letter is b
-						b[i] = (char) (sub);
+						while(newkey > 90) {
+						newkey = (newkey - 90) + 64; // rotating the digit for ex: if letter z and key is 2 then new letter is b
+						}
+						b[i] = (char) (newkey);
 					} else {
 						b[i] = (char) (newkey);
 					}
 				} else if (c >= 97 && c <= 122) {
 					int newkey = (num + key);
 					if (newkey > 122) {
-						int sub = (newkey - 122) + 96;
-						b[i] = (char) (sub);
+						while(newkey > 122) {
+						newkey = (newkey - 122) + 96;
+						}
+						b[i] = (char) (newkey);
 					} else {
 						b[i] = (char) (newkey);
 					}
