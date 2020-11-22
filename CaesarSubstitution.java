18b84d8aaadc1914100c1acd999e10208d34fab8
venkata vikas <52953086+venkatavikas69@users.noreply.github.com>
2020-11-22 20:55:48 +0530

    Update CaesarSubstitution.java

--- CaesarSubstitution.java
+++ CaesarSubstitution.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class CaesarSubstitution {
+public class DayScholar {
 	public static void main(String[] args) {
-		
+
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter key of 26 Characters: ");
 		char[] key = in.nextLine().toCharArray();
 		int keylen = key.length;
-		
-		while(keylen != 26) {
+
+		while (keylen != 26) {
 			main(args);
 		}
 		char[] small = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
@@ -22,19 +22,27 @@ public class CaesarSubstitution {
 		int a = 0;
 
 		for (int j = 0; j < msglen; j++) {
-			char c = msg[j];	//get char at postion j in msg
+			char c = msg[j]; // get char at postion j in msg
 			if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
 				if (c >= 65 && c <= 90) {
 					for (int i = 0; i < 26; i++) {
-						if (c == big[i]) {		//get the postion of j'th char in big array
-							newmsg[a] = key[i];	//set the char of newmsg to the char at position j to the char at postion i in key
+						if (c == big[i]) { // get the postion of j'th char in big array
+							if (key[i] >= 65 && key[i] <= 90) {
+								newmsg[a] = key[i]; // set the char of newmsg to the char at position j to the char at postion i in key
+							} else {
+								newmsg[a] = (char)(key[i] - 32); 
+							}
 							a++;
 						}
 					}
 				} else if (c >= 97 && c <= 122) {
 					for (int i = 0; i < 26; i++) {
 						if (c == small[i]) {
-							newmsg[a] = (char) (key[i] + 32); //add 32 to convert from lowercase to uppercase
+							if (key[i] >= 65 && key[i] <= 90) {
+								newmsg[a] = (char) (key[i] + 32); // add 32 to convert from uppercase to lowercase
+							} else {
+								newmsg[a] = key[i];
+							}
 							a++;
 						}
 					}
